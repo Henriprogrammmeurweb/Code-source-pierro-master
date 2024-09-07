af17ddfee863acc5fb76cf9038b6430a89b5c4d1
KIUKA HENRI ALIAS HENRI PROGRAMMEUR WEB <comptedepotgithub@gmail.com>
2024-09-07 19:06:52 +0200

    Ajout du pied de page

--- ProjetGestionPersonnel/settings.py
+++ ProjetGestionPersonnel/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5)@36+lhn7g^6^m4rz4()&mrfvxtx99uz7gn#lcnnfg@8wrf$^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
